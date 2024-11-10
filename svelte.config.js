df979a061c230f04db7e98136ce3f110eb8294f0
Artur <arthur.sogomonyan@gmail.com>
2024-11-10 03:59:53 +0100

    deploy fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,59 @@
-// import adapter from '@sveltejs/adapter-auto';
+// // import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-static';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+// 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+// 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+// 		adapter: adapter()
+// 	}
+// };
+
+// export default config;
+
+
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+// const base = process.env.BASE_PATH ?? '/arterra';
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter({
+			// fallback: '404.html'
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html'
+		}),
+		paths: {
+			// base,
+			// base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: dev ? '' : process.env.BASE_PATH ?? '/pro-concierge'
+			// base: process.env.BASE_PATH ?? '/arterra'
+        },
+		prerender: {
+			handleHttpError: ({ status, path, referrer }) => {
+				if (status === 404) {
+					console.warn(`Path not found: ${path}, referrer: ${referrer}`);
+					return {
+						ignore: true // Ignore 404s during prerendering
+					};
+				}
+			}
+		}
 	}
 };
 
 export default config;
+
